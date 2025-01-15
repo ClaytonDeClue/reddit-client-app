841dc6cfe65183b98eb8bff7429401470c891989
declc <clayton.declue@leaseplan.com>
2025-01-15 15:03:23 -0500

    Build Header Component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+    return (
+        <header className="header">
+            <div className="header-content">
+                <img 
+                src="reddit-logo-removebg-preview.png"
+                alt="Reddit App Logo"
+                className="header-logo"
+                />
+                <h1 className="header-title">Reddit<span className="reddit-color">Lite</span></h1>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
